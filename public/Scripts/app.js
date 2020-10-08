bd8a36eb3c5f051ed7eccf3045853079c4ce2ce2
301083645 <eblee150@gmail.com>
2020-10-07 21:56:25 -0400

    Upto Site Structure

---
+++ public/Scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE - Immediately Invoked function expression
+(function(){
+
+function Start()
+{
+    console.log("App started...");
+}
+
+window.addEventListener("load", Start);
+
+})();
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+  
+  { title: 'Eunbee Lee',
+    MainHeading: 'Main Heading' });
 });
 
 module.exports = router;
