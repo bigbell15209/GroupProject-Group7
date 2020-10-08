d77704f32c19b7177d986c8c8e4143824102920c
301083645 <eblee150@gmail.com>
2020-10-07 20:04:21 -0400

    Add routing /info & /contact

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!!');
+})
+
+app.get('/info', (req, res) => {
+  res.send('Site Info');
+})
+
+app.get('/contact', (req, res) => {
+  res.send('Contact Us');
 })
 
 app.listen(port, () => {
