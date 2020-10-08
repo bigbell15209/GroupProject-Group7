187af05e8f8a5a4dfae34271dc176dd7c89e52b0
301083645 <eblee150@gmail.com>
2020-10-07 22:09:26 -0400

    add some routes

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', 
-  
-  { title: 'Eunbee Lee',
-    MainHeading: 'Main Heading' });
+  res.render('index',{ title: 'Eunbee Lee',});
 });
+/* GET About Me page. */
+router.get('/about', function(req, res, next) {
+  res.render('index',{ title: 'About Me',});
+});
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index',{ title: 'Projects',});
+});
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index',{ title: 'Services',});
+});
+/* GET Contact Me page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index',{ title: 'Contact Me',});
+});
+
 
 module.exports = router;
