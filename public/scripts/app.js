1f3ccdf222427346bdc544ed67bb46c76a895dee
Hankyu Shin <brad0501@naver.com>
2020-10-09 17:38:45 -0400

    slkafj

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+(function(){
+    function Start()
+    {
+        console.log("App Started...");
+      if(document.title =="Contact")
+     {
+      console.log("On the Contact page");
+     }
+    }
+
+})
