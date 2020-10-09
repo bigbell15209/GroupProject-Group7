443568eea54c3f7d4764fe8438aafa9e524c0c56
Hankyu Shin <brad0501@naver.com>
2020-10-09 17:43:13 -0400

    button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,8 +4,20 @@
         console.log("App Started...");
       if(document.title =="Contact")
      {
-      console.log("On the Contact page");
-     }
+      let sendButton = document.getElementById("sendButton");      
+      let cancelButton = document.getElementById("cancelButton");
+      
+      sendButton.addEventListener("click", (event) =>{
+          event.preventDefault();
+      });
+      cancelButton.addEventListener("click", (event) =>{
+        event.preventDefault();
+        if(confirm("Are you sure?"))
+        {
+            location.href = "/home"
+        }
+    });
+    }
     }
 
 })
