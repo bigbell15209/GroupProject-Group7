95b28c5aaa2bc201d40bd02b4b1e2b9288b52ff4
301083645 <eblee150@gmail.com>
2020-10-23 17:41:46 -0400

    change db URI to MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/application"
+    // local db
+    //"URI": "mongodb://localhost/application"
+    // mongo atlas // password : TwZzMjLIiXZ29Pu5
+    "URI" : "mongodb+srv://eunbee:TwZzMjLIiXZ29Pu5@mongodbserver.lt38h.mongodb.net/application?retryWrites=true&w=majority"
 }
