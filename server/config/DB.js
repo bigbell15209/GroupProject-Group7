69f28971a1d0fca5d864cfa3b005a5c2e1b524e3
Hankyu Shin <brad0501@naver.com>
2020-10-24 22:27:33 -0400

    project

--- server/config/DB.js
+++ server/config/DB.js
@@ -1,1 +1,1 @@
-module.exports.URL = "mongodb+srv://thomas:Hq3DKYN2NaAqGyyV@mongodbserver.k15hk.azure.mongodb.net/book_store?retryWrites=true&w=majority"
+module.exports.URL = "mongodb+srv://HK:<password>@mongodbserver.azeun.mongodb.net/<dbname>?retryWrites=true&w=majority"
