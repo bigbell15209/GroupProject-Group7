f0b2830720aa3f50fe758bd1c7a06cfea41d0ac9
Jiye Yu <jiye9701@gmail.com>
2020-12-04 08:37:50 -0500

    changed aboutus,add,edit,css by jiye2

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -87,25 +87,3 @@ window.addEventListener("load", Start);
 })();
 
 //j
-
-
-
-
-
-(function(){
-    $(".btn-outline-disable").click(function(event){
-      if(!confirm("Do you want to make it disable??")) {
-        event.preventDefault();
-        window.location.assign('/survey-list');
-      }
-    });
-
-
-    window.onload = function disable() {
-        var boardDisable = document.getElementById('boardDisable');
-        boardDisable.onclick = function() { alert('Sorry'); 
-        boardDisable.onclick = null;
-        window.location.assign('/home');
-        }
-    };
-    })()
